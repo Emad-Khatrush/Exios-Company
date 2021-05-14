d38b53dda992c4665f96b33b7e65256fda2cb79f
emadkhatrush <qwe.emad@hotmail.com>
2021-05-14 22:42:36 +0300

    finish v1 app

---
+++ app.js
@@ -0,0 +1,44 @@
+if(process.env.NODE_ENV !== "production"){
+    require("dotenv").config();
+  }
+  
+const express    = require('express'),
+    bodyParser   = require("body-parser"),
+    flash        = require("connect-flash"),
+    session      = require("express-session"),
+    app          = express();
+
+const port = process.env.PORT || 3000;
+
+// configure packages
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.set("view engine", "ejs");
+app.use(flash());
+app.use(session({ cookie: { maxAge: 60000 }, 
+  secret: 'alsana',
+  resave: false, 
+  saveUninitialized: false}));
+  
+app.use(function(req,res,next){
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
+});
+
+//-------------------------------
+//            Routes
+//-------------------------------
+// require routers
+const indexRoute = require("./routers/index");
+
+app.use(indexRoute);
+
+app.get("*", (req ,res) => {
+  res.redirect("/");
+});
+
+// add listening server
+app.listen(port, () => {
+    console.log("Exios app listening at http://localhost:" + port);
+});
---
+++ routers/index.js
@@ -0,0 +1,51 @@
+const express    = require("express"),
+      nodemailer = require('nodemailer'),
+      router  = express.Router();
+      
+const langAR  = require("../lang/ar");
+const langEN  = require("../lang/en");
+
+// home route
+router.get("/", (req, res) => {
+    res.render('home', { lang: langAR });
+});
+// home lang route
+router.get("/:lang", (req, res) => {
+    if(req.params.lang === "en"){
+        return res.render('home', { lang: langEN });
+    }else{
+        return res.redirect("/");
+    }
+});
+// contact us
+router.post("/:lang/sendMessage", (req, res) => {
+    var transporter = nodemailer.createTransport({
+        service: 'hotmail',
+        auth: {
+          user: 'expotimbuildinglibya@hotmail.com',
+          pass: process.env.HOTMAIL_PASS
+          }
+        });
+    
+        var mailOptions = {
+          from: 'expotimbuildinglibya@hotmail.com',
+          to: 'qwe.emad@hotmail.com',
+          subject: req.body.subject,
+          html: `<h3> Message from ${req.body.fullName}</h3> <br>
+          <p><strong> phone:</strong> ${req.body.phone || "Empty"} </p>
+          <p><strong> Email: </strong>${req.body.email || "Empty"} </p>
+          <p><strong> Message:</strong> ${req.body.message} </p>`
+        };
+    
+        transporter.sendMail(mailOptions, function(error, info){
+          if (error) {
+            console.log(error);
+            res.status(404).send();
+          } else {
+            console.log('Email sent: ' + info.response);
+            req.flash("success", ".تم ارسال الرسالة بنجاح");
+            res.redirect("/" + req.params.lang);
+          }
+        });
+});
+module.exports = router;
